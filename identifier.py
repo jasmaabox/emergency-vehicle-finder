77345effe10e47a3876a2d853e764be01cb79ef0
jasmaa <jasonmaa3955@gmail.com>
2018-10-16 08:02:14 -0400

    Tweak classifier, add video support

--- identifier.py
+++ identifier.py
@@ -1,20 +1,43 @@
 import os
 import cv2
 
-def process_img(img):
-    """Returns array of locations of emergency vehicles"""
-    # ADD OTHER CLASSIFIERS
-
-    firetrucks = firetruck_haar.detectMultiScale(img, scaleFactor=1.2, minNeighbors=5)
-
-    emergency_vehicles = firetrucks
-    emergency_vehicles = list(map(lambda x: [x[0], x[1]], emergency_vehicles))
-
-    return emergency_vehicles
-
+SHOW_VIDEO = True
 
 # load classifiers
 firetruck_haar = cv2.CascadeClassifier('data/firetruck_cascade_5.xml')
+ems_haar = cv2.CascadeClassifier('data/ambulance_cascade.xml')
+
+def find_center(x):
+    """Finds center of roi"""
+    return [int(x[0]+x[2]/2), int(x[1]+x[3]/2)]
+
+
+def process_img(img):
+    """Returns array of locations of emergency vehicles"""
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    # ADD OTHER CLASSIFIERS
+
+    firetrucks = firetruck_haar.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=10)
+    ems = []#ems_haar.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=10)
+    police = []
+
+    # show vid
+    if SHOW_VIDEO:
+        for (x, y, w, h) in firetrucks:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 255), 2)
+        for (x, y, w, h) in ems:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.imshow('frame', img)
+        cv2.waitKey(1)
+
+    emergency_vehicles = []
+    emergency_vehicles += list(map(find_center, firetrucks))
+    emergency_vehicles += list(map(find_center, ems))
+    emergency_vehicles += list(map(find_center, police))
+    
+    return emergency_vehicles
+
 
 def main():
     # load files
@@ -26,8 +49,7 @@ def main():
         
         if f[-3:].lower() == 'bmp':
             img = cv2.imread(f)
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            res = process_img(gray)
+            res = process_img(img)
 
             print('\n'.join([str(len(res)), str(res)]))
             
@@ -41,8 +63,7 @@ def main():
                     cap.release()
                     break
                 
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                res = process_img(gray)
+                res = process_img(frame)
                 loc_arr.append(res)
                 num_arr.append(len(res))
                 
@@ -53,3 +74,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+        
